dbf07c567eaef219c843460b4e1f00502a80e622
Nathan Lilienthal <nathan@nixpulvis.com>
2023-02-23 17:09:16 -0500

    Initial Commit

---
+++ Nix Square Watch App/Nix_SquareApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nix_SquareApp.swift
+//  Nix Square Watch App
+//
+//  Created by Nathan Lilienthal on 2/23/23.
+//
+
+import SwiftUI
+
+@main
+struct Nix_Square_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Nix Square Watch AppUITests/Nix_Square_Watch_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Nix_Square_Watch_AppUITestsLaunchTests.swift
+//  Nix Square Watch AppUITests
+//
+//  Created by Nathan Lilienthal on 2/23/23.
+//
+
+import XCTest
+
+final class Nix_Square_Watch_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
